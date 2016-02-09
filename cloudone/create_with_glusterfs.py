184f6dd7fcdc387ba3776fb0ef7a906cdcfaf151
edcyc <edcyc.tw@gmail.com>
2016-02-09 16:03:08 -0600

    Change to new version

--- cloudone/create_with_glusterfs.py
+++ cloudone/create_with_glusterfs.py
@@ -10,6 +10,18 @@ os.system("kubectl create -f glusterfs-endpoints.json")
 # create an glusterfs service
 os.system("kubectl create -f glusterfs-service.json --validate=false")
 
+# create an etcd endpoint
+os.system("kubectl create -f etcd-endpoints.json")
+
+# create an etcd service
+os.system("kubectl create -f etcd-service.json --validate=false")
+
+# create an etcd endpoint
+os.system("kubectl create -f kubeapi-endpoints.json")
+
+# create an etcd service
+os.system("kubectl create -f kubeapi-service.json --validate=false")
+
 # create a replication controller to replicate nodes
 os.system("kubectl create -f cloudone-controller-glusterfs.json")
 
